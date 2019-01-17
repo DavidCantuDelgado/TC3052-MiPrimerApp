bbf5c26d25de40c785099a76933e56eb5c9ed154
David Bugsracer Cantú <david@bugsracer.com>
2019-01-17 01:12:00 -0600

    Initial Commit

---
+++ TC3052-MiPrimerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TC3052-MiPrimerApp
+//
+//  Created by David A Cantú Delgado on 1/17/19.
+//  Copyright © 2019 Tec de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
